9e7cd8d4ec59b399dadf17435cf49a6dcc4f4478
ekucukkirli <56414732+ekucukkirli@users.noreply.github.com>
2021-12-26 00:33:56 +0300

    added images and create folders

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.jpg" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.jpg" type="image/x-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"/>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Password Generator</title>
 </head>
 <body class="dark">
@@ -85,6 +85,6 @@
                   <button onclick="copyFun()" id="copy_btn">Copy Password</button>
             </div>
       </div>
-      <script src="script.js"></script>
+      <script src="js/script.js"></script>
 </body>
 </html>
